023f6f547a347022c72819c21a2f7e209224870f
law12th <lawrencetepula@gmail.com>
2022-10-01 07:56:15 +0200

    feat: export errors and middlewares

--- packages/ult-utils/index.ts
+++ packages/ult-utils/index.ts
@@ -1,2 +1,11 @@
+export * from "./errors/BadRequestError";
+export * from "./errors/InternalServerError";
+export * from "./errors/NotFoundError";
+export * from "./errors/RequestValidationError";
+export * from "./errors/UnauthorizedError";
+
 export { default as logger } from "./logging";
 export { default as morganMiddleware } from "./logging/morganMiddleware";
+
+export * from "./middlewares/errorHandler";
+export * from "./middlewares/validateRequest";
